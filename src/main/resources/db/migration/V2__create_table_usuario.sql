20d5663e49ffe56b040dc8aa23b5939576d4fc9b
Gui-Coimbra <guilherme.brito@sptech.school>
2024-01-25 14:20:08 -0300

    feat: First commit

---
+++ src/main/resources/db/migration/V2__create_table_usuario.sql
@@ -0,0 +1,9 @@
+create table usuario(
+    id        bigint not null auto_increment,
+    nome      varchar(50) not null ,
+    email varchar(50) not null ,
+    primary key (id)
+);
+
+insert into usuario(id, nome, email)
+values (1, 'Ana da Silva', 'ana@email.com');
