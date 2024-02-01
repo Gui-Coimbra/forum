079aef1e458d048f33867c0ee3a526ac52c46ace
Gui-Coimbra <guilherme.brito@sptech.school>
2024-02-01 16:49:47 -0300

    feat: Configurando geração, validação e autenticação de token - jwt

--- src/main/kotlin/br/com/alura/forum/config/SecurityConfiguration.kt
+++ src/main/kotlin/br/com/alura/forum/config/SecurityConfiguration.kt
@@ -1,7 +1,10 @@
 package br.com.alura.forum.config
 
+import br.com.alura.forum.security.JWTAuthenticationFilter
+import br.com.alura.forum.security.JWTLoginFilter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
@@ -9,25 +12,29 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.web.filter.OncePerRequestFilter
 
 @Configuration
 @EnableWebSecurity
 class SecurityConfiguration(
-    private val userDetailService: UserDetailsService
+    private val userDetailService: UserDetailsService,
+    private val jwtUtil: JWTUtil
 ) : WebSecurityConfigurerAdapter() {
 
     override fun configure(http: HttpSecurity) {
         http?.
+        csrf()?.disable()?.
         authorizeRequests()?.
-            antMatchers("/topicos")?.hasAuthority("LEITURA_ESCRITA")?.
+        antMatchers("/topicos")?.hasAuthority("LEITURA_ESCRITA")?.
+        antMatchers(HttpMethod.POST, "/login")?.permitAll()?.
         anyRequest()?.
         authenticated()?.
-        and()?.
-        sessionManagement()?.
-        sessionCreationPolicy(SessionCreationPolicy.STATELESS)?.
-        and()?.
-        formLogin()?.disable()?.
-        httpBasic()
+        and()
+        http?.addFilterBefore(JWTLoginFilter(authManager = authenticationManager(), jwtUtil = jwtUtil), UsernamePasswordAuthenticationFilter().javaClass)
+        http?.addFilterBefore(JWTAuthenticationFilter(jwtUtil = jwtUtil), UsernamePasswordAuthenticationFilter().javaClass)
+        http.sessionManagement()?.
+        sessionCreationPolicy(SessionCreationPolicy.STATELESS)
     }
 
     @Bean
